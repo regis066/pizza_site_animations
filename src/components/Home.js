d9385edd73ba12c5cf40cf9cc36dde04c028cbb3
Mugisha Regis <mugisharegis72@gmail.com>
2023-12-24 17:41:36 +0200

    framer motion basic properties usage

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+const Home = () => {
+  return (
+    <motion.div className="home container" initial={{ opacity: 0}} animate={{ opacity: 1 }} transition={{ duration: 1.5, delay: 1.5}}>
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to="/base">
+        <motion.button whileHover={{ scale: 1.1 , textShadow: '0px 0px 8px rgb(255,255,255)', boxShadow: '0px 0px 8px rgb(255,255,255)'}}>Create Your Pizza</motion.button>
+      </Link>
+    </motion.div>
+  );
+}
+
+export default Home;
