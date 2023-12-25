96e54aee17a97717ffb22895d3a2178f9f1d519b
Mugisha Regis <mugisharegis72@gmail.com>
2023-12-25 16:38:40 +0200

    framer motion keyframes usage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+
+const buttonVariants = {
+  hover: {
+    scale: [ 1 , 1.1 , 1 , 1.1 , 1 , 1.1 , 1],
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+  },
+};
 const Home = () => {
   return (
-    <motion.div className="home container" initial={{ opacity: 0}} animate={{ opacity: 1 }} transition={{ duration: 1.5, delay: 1.5}}>
+    <motion.div
+      className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5, delay: 1.5 }}
+    >
       <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button whileHover={{ scale: 1.1 , textShadow: '0px 0px 8px rgb(255,255,255)', boxShadow: '0px 0px 8px rgb(255,255,255)'}}>Create Your Pizza</motion.button>
+        <motion.button
+          variants={buttonVariants}
+          whileHover="hover"
+        >
+          Create Your Pizza
+        </motion.button>
       </Link>
     </motion.div>
   );
-}
+};
 
 export default Home;
